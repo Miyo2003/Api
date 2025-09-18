400ab7ef3d3a620e41fd290c5775a4ed6c0db738
Miyo2003 <edsharclee@gmail.com>
2025-09-18 11:52:37 +0800

    uploading

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class HelloResponse(BaseModel):
+    message: str
+
+@app.get("/hello", response_model=HelloResponse)
+def get_hello():
+    return HelloResponse(message="Hello World")
+
+# You can run this app with:
+# uvicorn app.main:app --reload
--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
-
-class HelloResponse(BaseModel):
-    message: str
-
-@app.get("/hello", response_model=HelloResponse)
-def get_hello():
-    return HelloResponse(message="Hello World")
-
-# You can run this app with:
-# uvicorn main:app --reload
