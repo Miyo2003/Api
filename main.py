ddebbf60eaa28df096109f4fe0f53767f64beec5
Miyo2003 <edsharclee@gmail.com>
2025-09-20 16:41:55 +0800

    uploading

--- main.py
+++ main.py
@@ -1,160 +1,16 @@
+then modify this 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
-# Replace root endpoint with ASCII art
-@app.get("/", response_class=PlainTextResponse)
+@app.get("/")
 def read_root():
-    art = """
+    return {"message": "Hello World"}
 
-    ♥ Hello World♥
- 
-  ♥♥♥♥♥  
- ♥        
- ♥        
-  ♥♥♥♥   
-       ♥ 
-       ♥ 
- ♥♥♥♥♥♥♥ 
-
- 
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-
- 
- ♥♥♥♥♥♥♥
-    ♥   
-    ♥   
-    ♥   
-    ♥   
-    ♥   
-    ♥   
-
- 
- ♥♥♥♥♥♥♥
-    ♥   
-    ♥   
-    ♥   
-    ♥   
-    ♥   
-    ♥   
-
- 
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-
- 
- ♥♥♥♥♥  
- ♥    ♥ 
- ♥    ♥ 
- ♥♥♥♥♥  
- ♥ ♥    
- ♥  ♥   
- ♥   ♥  
-
- 
-   ♥   
-  ♥ ♥  
- ♥   ♥ 
- ♥♥♥♥♥ 
- ♥   ♥ 
- ♥   ♥ 
- ♥   ♥ 
-
- 
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-   ♥   
-
- 
- ♥     ♥
- ♥♥    ♥
- ♥ ♥   ♥
- ♥  ♥  ♥
- ♥   ♥ ♥
- ♥    ♥♥
- ♥     ♥
-
- 
-   ♥   
-  ♥ ♥  
- ♥   ♥ 
- ♥♥♥♥♥ 
- ♥   ♥ 
- ♥   ♥ 
- ♥   ♥ 
-
- 
- ♥     ♥
- ♥♥   ♥♥
- ♥ ♥ ♥ ♥
- ♥  ♥  ♥
- ♥     ♥
- ♥     ♥
- ♥     ♥
-
- 
-   ♥   
-  ♥ ♥  
- ♥   ♥ 
- ♥♥♥♥♥ 
- ♥   ♥ 
- ♥   ♥ 
- ♥   ♥ 
-
- 
- ♥      
- ♥      
- ♥      
- ♥      
- ♥      
- ♥      
- ♥♥♥♥♥♥♥
-
- 
-   ♥   
-  ♥ ♥  
- ♥   ♥ 
- ♥♥♥♥♥ 
- ♥   ♥ 
- ♥   ♥ 
- ♥   ♥ 
-
- 
- ♥     ♥
- ♥♥    ♥
- ♥ ♥   ♥
- ♥  ♥  ♥
- ♥   ♥ ♥
- ♥    ♥♥
- ♥     ♥
-"""
-    return art
-
-# Keep your HelloResponse as is
 class HelloResponse(BaseModel):
     message: str
 
 @app.get("/hello", response_model=HelloResponse)
 def get_hello():
     return HelloResponse(message="Hello World")
-
-
-# You can run this app with:
-# uvicorn main:app --reload
